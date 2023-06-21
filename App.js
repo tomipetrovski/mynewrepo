43a9a169d66fa6913304523b4d813350d024faed
tomipetrovski <tp30566@seeu.edu.mk>
2023-06-21 13:28:52 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {StyleSheet, View, Text} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ProductsList } from "./screens/ProductsList.js";
+import { ProductDetails } from "./screens/ProductDetails.js";
+import { Cart } from "./screens/Cart.js";
+import { CartProvider } from "./CartContext.js";
+import { CartIcon } from "./components/CartIcon.js";
+
+
+const Stack = createNativeStackNavigator();
+
+function App(){
+  return(
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Products" component={ProductsList} options={({navigation}) => ({title: 'Products', headerRight: () => <CartIcon navigation={navigation} />})} />
+          <Stack.Screen name="ProductDetails" component={ProductDetails} options={({navigation}) => ({title: 'Products', headerRight: () => <CartIcon navigation={navigation} />})} />
+          <Stack.Screen name="Cart" component={Cart} options={({navigation}) => ({title: 'Products', headerRight: () => <CartIcon navigation={navigation} />})} />
+          <View>
+            {categoriesData.map((category) => (
+              <CategoryItem key={category.id} category={category} />
+            ))}
+          </View>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
+
+export default App;
